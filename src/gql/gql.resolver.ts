f642a04435c0643606b0411ae287a8f12fb62159
ADubey <email2amitdubey@gmail.com>
2024-10-04 19:25:36 +0530

    2nd order

--- src/gql/gql.resolver.ts
+++ src/gql/gql.resolver.ts
@@ -69,7 +69,6 @@ export class GqlResolver {
     return genes;
   }
 
-
   @Query((returns) => [GeneInteractionOutput])
   async getGeneInteractions(
     @Args('input') input: InteractionInput,
@@ -91,7 +90,7 @@ export class GqlResolver {
         WHERE r.score >= $minScore AND g2.ID IN geneIDs
         RETURN COLLECT({gene1: g1.ID, gene2: g2.ID, score: r.score}) AS connections, genes
         `;
-    } else if (order === 1) {
+    } else {
       query = `
         WITH $geneIDs AS geneIDs
         MATCH (g1:Gene)-[r:${interactionType}]->(g2:Gene)
@@ -100,49 +99,71 @@ export class GqlResolver {
         WITH apoc.coll.toSet(COLLECT(g1) + COLLECT(g2)) AS _genes, COLLECT({gene1: g1.ID, gene2: g2.ID, score: r.score}) AS _connections
         RETURN _genes[0..${process.env.NODES_LIMIT || 5000}] AS genes, _connections[0..${process.env.EDGES_LIMIT || 10000}] AS connections
       `;
-    } else {
-      query = `
-        WITH $geneIDs AS geneIDs
-        MATCH (g1:Gene)-[r1:${interactionType}]->(g2:Gene)-[r2:${interactionType}]->(g3:Gene)
-        WHERE g1.ID IN geneIDs
-        AND r1.score >= $minScore AND r2.score >= $minScore
-        WITH apoc.coll.toSet(COLLECT(g1) + COLLECT(g2) + COLLECT(g3)) AS _genes, COLLECT({gene1: g1.ID, gene2: g2.ID, score: r1.score}) + COLLECT({gene1: g2.ID, gene2: g3.ID, score: r2.score}) AS _connections
-        RETURN _genes[0..${process.env.NODES_LIMIT || 5000}] AS genes, _connections[0..${process.env.EDGES_LIMIT || 10000}] AS connections
-      `;
     }
+    //     } else {
+    //       //       query = `MATCH (g:Gene)
+    //       // WHERE g.ID IN $geneIDs
+    //       // RETURN g.ID, g LIMIT 10`;
+    //       //       query = `WITH $geneIDs AS geneIDs
+    //       // MATCH (g1:Gene)-[r1:GENE_GENE_CONNECTION]->(g2:Gene)
+    //       // WHERE g1.ID IN geneIDs
+    //       //   AND r1.score >= $minScore
+    //       // WITH COLLECT(g2) AS firstOrderGenes, COLLECT(g1.ID) AS inputGenes
+    //       // UNWIND firstOrderGenes AS g2
+    //       // MATCH (g2)-[r2:GENE_GENE_CONNECTION]->(g3:Gene)
+    //       // WHERE r2.score >= $minScore
+    //       //   AND NOT g3.ID IN inputGenes // Exclude original input genes
+    //       // WITH COLLECT(g3) AS secondOrderGenes, COLLECT({gene1: g2.ID, gene2: g3.ID, score: r2.score}) AS connections
+    //       // RETURN COLLECT(secondOrderGenes) AS genes, connections`;
+    //       query = `
+    // WITH $geneIDs AS geneIDs
+    //       MATCH (g1:Gene)-[r1:GENE_GENE_CONNECTION]->(g2:Gene)
+    //       WHERE g1.ID IN geneIDs
+    //         AND r1.score >= $minScore
+    //       WITH COLLECT(g2) AS firstOrderGenes, COLLECT(g1.ID) AS inputGenes
+
+    //       UNWIND firstOrderGenes AS g2
+    //       MATCH (g2)-[r2:GENE_GENE_CONNECTION]->(g3:Gene)
+    //       WHERE r2.score >= $minScore
+    //         AND NOT g3.ID IN inputGenes
+    //       WITH COLLECT(g3) AS genes, COLLECT({gene1: g2.ID, gene2: g3.ID, score: r2.score}) AS connections
+    //       RETURN genes[0..${process.env.NODES_LIMIT || 5000}] AS genes, connections[0..${process.env.EDGES_LIMIT || 10000}] AS connections
+    //     `;
+    //     }
     const result: QueryResult<any> = await session.run(query, {
       geneIDs,
       minScore,
     });
-    const genes: Gene[] = result.records?.[0]?.get('genes')?.map((gene) => {
-      gene = gene.properties;
-      gene.common = {};
-      diseaseNames.forEach((disease) => {
-        gene[disease] = {};
-      });
-      for (const key in gene) {
+    const genes: Gene[] =
+      result.records?.[0]?.get('genes')?.map((gene) => {
+        gene = gene.properties;
+        gene.common = {};
         diseaseNames.forEach((disease) => {
+          gene[disease] = {};
+        });
+        for (const key in gene) {
+          diseaseNames.forEach((disease) => {
+            if (
+              key.startsWith(`${disease}_GWAS`) ||
+              key.startsWith(`${disease}_GDA`) ||
+              key.startsWith(`${disease}_logFC`) ||
+              key.startsWith(`${disease}_database`)
+            ) {
+              gene[disease][key.slice(disease.length + 1)] = gene[key];
+              delete gene[key];
+            }
+          });
           if (
-            key.startsWith(`${disease}_GWAS`) ||
-            key.startsWith(`${disease}_GDA`) ||
-            key.startsWith(`${disease}_logFC`) ||
-            key.startsWith(`${disease}_database`)
+            key.startsWith(`pathway_`) ||
+            key.startsWith(`Druggability_`) ||
+            key.startsWith(`TE_`)
           ) {
-            gene[disease][key.slice(disease.length + 1)] = gene[key];
+            gene.common[key] = gene[key];
             delete gene[key];
           }
-        });
-        if (
-          key.startsWith(`pathway_`) ||
-          key.startsWith(`Druggability_`) ||
-          key.startsWith(`TE_`)
-        ) {
-          gene.common[key] = gene[key];
-          delete gene[key];
         }
-      }
-      return gene;
-    }) ?? [];
+        return gene;
+      }) ?? [];
 
     const indexMap = genes.reduce((acc, gene, index) => {
       acc[gene.ID] = index;
@@ -155,7 +176,7 @@ export class GqlResolver {
       `Found ${genes.length} genes and ${connections.length} links.`,
     );
 
-    return {
+    let finalResult = {
       genes: genes,
       links: connections.map((link) => ({
         gene1: {
@@ -169,5 +190,23 @@ export class GqlResolver {
         score: link.score,
       })),
     };
+    if (order === 2) {
+      let res = await this.getGeneInteractions(
+        {
+          geneIDs: genes.map((gene) => gene.ID),
+          minScore,
+          interactionType,
+        },
+        0,
+        diseaseNames,
+      );
+      finalResult = {
+        genes: [...new Set(res.genes)],
+        links: [...new Set(res.links)],
+        // links: [...new Set(finalResult.links.concat(res.links))],
+      };
+    }
+    console.log(finalResult.genes.length);
+    return finalResult;
   }
 }
